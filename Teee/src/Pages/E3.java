b0d4bfd76aca4db82c10d16a28fe59ca3fcf3af0
atul123 <abharadwajj@gmail.com>
2018-09-03 12:38:48 +0530

    added m

--- Teee/src/Pages/E3.java
+++ Teee/src/Pages/E3.java
@@ -5,7 +5,7 @@ public class E3 {
 	public static void main(String[] args) {
 		System.out.println(1);
 		System.out.println(1.1);
-		System.out.println(1.2);
+		System.out.println(1.2+"---"+1.3);
 
 	}
 
