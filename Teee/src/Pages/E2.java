13b8007d26151688673e0ddbb94c9df19ca87ebb
atul123 <abharadwajj@gmail.com>
2018-09-03 11:56:11 +0530

    added 1

--- Teee/src/Pages/E2.java
+++ Teee/src/Pages/E2.java
@@ -4,9 +4,7 @@ public class E2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Sade wade");
-System.out.println("Sade wade sade");
-		//new comment
+System.out.println(1);
 	}
 
 }
